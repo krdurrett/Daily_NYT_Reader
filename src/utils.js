70df4086436e8ce2b3e06998ce28f7d8f4f30ae9
Kayla Durrett <kayla.durrett@gmail.com>
2022-04-19 13:20:00 -0600

    Fix helper function to account for a null multimedia

--- src/utils.js
+++ src/utils.js
@@ -8,17 +8,32 @@ export const handleResponse = (response) => {
 
 export const cleanData = (articles) => {
   return articles.map(article => {
-    return {
-      id: article.short_url,
-      category: article.section,
-      title: article.title,
-      abstract: article.abstract,
-      link: article.url,
-      byline: article.byline,
-      published: article.published_date,
-      image: article.multimedia[1].url,
-      imageTagline: article.multimedia[1].caption,
-      imageCopyright: article.multimedia[1].copyright
+    if (article.multimedia === null) {
+      return {
+        id: article.short_url,
+        category: article.section,
+        title: article.title,
+        abstract: article.abstract,
+        link: article.url,
+        byline: article.byline,
+        published: article.published_date,
+        image: '',
+        imageTagline: '',
+        imageCopyright: ''
+      }
+    } else {
+      return {
+        id: article.short_url,
+        category: article.section,
+        title: article.title,
+        abstract: article.abstract,
+        link: article.url,
+        byline: article.byline,
+        published: article.published_date,
+        image: article.multimedia[1].url,
+        imageTagline: article.multimedia[1].caption,
+        imageCopyright: article.multimedia[1].copyright
+      }
     }
   }) 
 }
