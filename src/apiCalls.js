76fafcc4d90d379ca5fd265b4e16494d80df6d5c
Kayla Durrett <kayla.durrett@gmail.com>
2022-04-19 12:11:35 -0600

    Add another call for more articles

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -3,7 +3,11 @@ import { handleResponse } from "./utils"
 const apiKey = process.env.REACT_APP_API_KEY
 
 export const getArtArticles = () => {
-  return fetch(`https://api.nytimes.com/svc/topstories/v2/arts.json?api-key=Gi1Af6N0Y7U6iBwAiWlZOICyAoL9BXHd`)
+  return fetch(`https://api.nytimes.com/svc/topstories/v2/arts.json?api-key=${apiKey}`)
     .then(response => handleResponse(response))
 }
 
+export const getMovieArticles = () => {
+  return fetch(`https://api.nytimes.com/svc/topstories/v2/movies.json?api-key=${apiKey}`)
+    .then(response => handleResponse(response))
+}
--- src/components/ArticleContainer.js
+++ src/components/ArticleContainer.js
@@ -1,13 +1,16 @@
 import React, { useEffect, useState } from 'react'
 import '../styles/ArticleContainer.css'
 import ArticleCard from './ArticleCard'
-import { getArtArticles } from '../apiCalls'
+import { getArtArticles, getMovieArticles } from '../apiCalls'
 
 const ArticleContainer = () => {
   const [articles, setArticles] = useState([])
-
+  
   useEffect(() => {
-    getArtArticles().then(data => setArticles(data.results))
+    const fetchedData = []
+    getArtArticles().then(data => data.results.forEach(result => fetchedData.push(result)))
+    getMovieArticles().then(data => data.results.forEach(result => fetchedData.push(result)))
+    setArticles(fetchedData)
   }, [])
   
   console.log(articles)
