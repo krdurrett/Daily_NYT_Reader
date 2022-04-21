6b00849f295ede8f84039f0e97873c97eccf1fda
Kayla Durrett <kayla.durrett@gmail.com>
2022-04-21 12:53:17 -0600

    Add styling to article detail page

--- src/components/ArticleCard.js
+++ src/components/ArticleCard.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import '../styles/ArticleCard.scss'
 
 const ArticleCard = ({ id, image, title, published, category }) => {
   return(
--- src/components/ArticleDetails.js
+++ src/components/ArticleDetails.js
@@ -4,20 +4,25 @@ import '../styles/ArticleDetails.scss'
 const ArticleDetails = ({ article }) => {
   return(
     <section className='article-details'>
-      <article className='text'>
+      <article className='title-text'>
         <h2>{article.title}</h2>
         <p>{article.byline}</p>
         <p>Published: {article.published}</p>
       </article>
       <article className='info'>
-        <img src={article.image} />
-        <p>{article.imageTagline}</p>
-        <p>{article.imageCopyright}</p>
-        <p>{article.abstract}</p>
-      <a href={article.link}>Link to article</a>
+        <div className='image-tag'>
+          <img src={article.image} />
+        </div>
+        <div className='overview'>
+          <div>
+            <p>{article.imageTagline}</p>
+            <p>{article.abstract}</p>
+          </div>
+          <a href={article.link}>Link to article</a>
+        </div>  
       </article>  
+      <p className='copy-right'>{article.imageCopyright}</p>
     </section>
-  
   )
 }
 
