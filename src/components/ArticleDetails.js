bd2683b889a7dd9bc026a6abd9007eea72463e7a
Kayla Durrett <kayla.durrett@gmail.com>
2022-04-21 10:45:53 -0600

    Add more styling

--- src/components/ArticleDetails.js
+++ src/components/ArticleDetails.js
@@ -3,16 +3,20 @@ import '../styles/ArticleDetails.scss'
 
 const ArticleDetails = ({ article }) => {
   return(
-    <>
-      <h2>{article.title}</h2>
-      <p>{article.byline}</p>
-      <p>Published: {article.published}</p>
-      <img src={article.image} />
-      <p>{article.imageTagline}</p>
-      <p>{article.imageCopyright}</p>
-      <p>{article.abstract}</p>
+    <section className='article-details'>
+      <article className='text'>
+        <h2>{article.title}</h2>
+        <p>{article.byline}</p>
+        <p>Published: {article.published}</p>
+      </article>
+      <article className='info'>
+        <img src={article.image} />
+        <p>{article.imageTagline}</p>
+        <p>{article.imageCopyright}</p>
+        <p>{article.abstract}</p>
       <a href={article.link}>Link to article</a>
-    </>
+      </article>  
+    </section>
   
   )
 }
